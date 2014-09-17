3099ffe584905cc40997e8ba2ec2d473c6892f73
chris <chrisd1891@gmail.com>
2014-09-16 19:45:58 -0700

    bump

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Finch.Mixfile do
 
   def project do
     [app: :finch,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.0.0",
      package: [
         contributors: ["Chris Duranti"],
