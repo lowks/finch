66962fdde361a2958ac963d3f4067862e86b8cf0
Chris Duranti <chrisd1891@gmail.com>
2014-10-19 15:19:35 -0700

    version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Finch.Mixfile do
 
   def project do
     [app: :finch,
-     version: "0.0.2",
+     version: "0.0.3",
      elixir: "~> 1.0.0",
      package: [
         contributors: ["Chris Duranti"],
